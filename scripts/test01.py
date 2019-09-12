6ee2accec34ac39b0c1919b9442c9d3134b58e1b
zxq <zxq1547080601@163.com>
2019-09-12 11:30:15 +0800

    first commit

---
+++ scripts/test01.py
@@ -0,0 +1,11 @@
+class Test01:
+    def test01(self):
+        print("test01被执行")
+
+    def test02(self):
+        print("test02被执行")
+
+    def test03(self):
+        print("test03被执行")
+
+        assert 0
